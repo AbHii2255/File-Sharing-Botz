aeddc3a6364a75f8961cdbf64f9cd370acb40d30
AbHii2255 <103345311+AbHii2255@users.noreply.github.com>
2022-11-26 10:58:49 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,17 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>ιAм Jυѕт A Aυтo Fɪʟᴇ Sʜᴀʀᴇ Boт..!🤩 Tнιѕ Boαт ιѕ Mαde Eхclυѕιvely For Tнe👉 Mαllυ Hυв Movιeѕ Groυp</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton('👉 𝐂𝐡𝐚𝐧𝐧𝐞𝐥', url='https://t.me/Mallushubb'),
+                        InlineKeyboardButton('👉 𝐆𝐫𝐨𝐮𝐩', url='https://t.me/Malluhubmovies')   
+                    ],
+                    [
+                        InlineKeyboardButton('👨🏻‍🔧 𝐂𝐫𝐞𝐚𝐭𝐨𝐫', url='https://t.me//Abhii238'),
+                        InlineKeyboardButton("🔒 𝐂𝐥𝐨𝐬𝐞", callback_data = "close")
                     ]
                 ]
             )
